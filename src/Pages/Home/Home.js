592b90136a28b8cfc8bd65d89c0a8f4005686d1f
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-10 02:51:52 +0600

    home page fixed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Banner from './Banner';
+import Exceptional from './Exceptional';
+import Footer from './Footer';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonial from './Testimonial';
+
 
 const Home = () => {
     return (
-        <div>
-            This is homepage
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <Exceptional></Exceptional>
+            <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
+            <Footer></Footer>
         </div>
     );
 };
